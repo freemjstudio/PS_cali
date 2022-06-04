78f60f4019e2f177101e7727600f1578fd996ff5
freemjstudio <mjwoo0@naver.com>
2022-06-04 13:57:04 -0700

    [Boj] 11779

--- 11779.py
+++ 11779.py
@@ -14,27 +14,28 @@ for _ in range(m): # 버스의 정보
     graph[a].append((b, cost))
 start, end = map(int, input().split()) # 출발도시 , 도착도시 
 queue = [(0, start)] # (cost, 출발도시)
-queue = heapq.heapify(queue) # 우선순위 큐 
-
-distance[start] = 0 
+heapq.heapify(queue) # 우선순위 큐 
+path[start].append(start)
 
 while queue:
     now_cost, now = heapq.heappop(queue)
     if now_cost > distance[now]:
-        continue
-    
+            continue
+    if now == start:
+        distance[now] = 0 
+
     for next, next_cost in graph[now]:
         temp_cost = next_cost + now_cost
         if temp_cost < distance[next]:
             distance[next] = temp_cost
             heapq.heappush(queue, (temp_cost, next))
             # path 가 갱신되었을 때 현재까지의 경로를 넣어준다. 
-            path[next] = []
+            path[next] = [] # 초기화해준다. 
             for i in path[now]:
                 path[next].append(i)
             path[next].append(next)
             
     
-print(distance[start]) # 최소비용
+print(distance[end]) # 최소비용
 print(len(path[end])) # 경로에 포함된 도시의 수 
 print(' '.join(map(str, path[end]))) # 경로 출력하기 
