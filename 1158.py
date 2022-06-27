7db9ed5ed65830e8c47fec46f544a880cef70931
freemjstudio <mjwoo0@naver.com>
2022-06-26 20:54:25 -0700

    1158

--- 1158.py
+++ 1158.py
@@ -1,9 +1,18 @@
 # 1158 요세푸스 
 
 n, k = map(int, input().split())
+queue = [i for i in range(1, n+1)] # 원형큐
+result = [] # 수행결과 저장 
+step = k-1
 
-queue = [i for i in range(1, n+1)]
+for i in range(n):
+    if len(queue) > step:
+        result.append(queue.pop(step)) # pop()은 element 를 return gka 
+        step += k-1
+    else: # len(queue) <= step 
+        step = step % len(queue)
+        result.append(queue.pop(step))
+        
+        step += k-1
 
-print(queue)
-
-print('<')
+print("<",", ".join(str(i) for i in result),">", sep="")
